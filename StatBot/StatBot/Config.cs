c4a4a98603d2b58c3d1ec2b0e48e062fdbcba8aa
Lane Sorell <lanesorell@gmail.com>
2020-05-12 16:03:38 -0700

    Initial commit

---
+++ StatBot/StatBot/Config.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatBot
+{
+    public class Config
+    {
+        [JsonProperty("riot_api_key")]
+        public string RiotAPIKey { get; set; }
+
+        [JsonProperty("discord_token")]
+        public string DiscordToken { get; set; }
+    }
+}
---
+++ StatBot/StatBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StatBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\secrets.json");
+            string token = JsonConvert.DeserializeObject<Config>(File.ReadAllText(path)).DiscordToken;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
